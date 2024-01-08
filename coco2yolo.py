8fcc861f380d93e0cc8417c189dd4c749cd46a40
UltralyticsAssistant <web@ultralytics.com>
2024-01-08 01:07:08 +0000

    Auto-format by Ultralytics actions

--- coco2yolo.py
+++ coco2yolo.py
@@ -3,17 +3,17 @@ from tqdm import tqdm
 import numpy as np
 import os
 
-#----------------------------------------------------------------------#
+# ----------------------------------------------------------------------#
 #   将coco格式的标注转换为yolo格式的标注
 #   不适用官方的coco json,官方的id从1开始,并且是90个类别,而平常用的是80个类别
-#----------------------------------------------------------------------#
+# ----------------------------------------------------------------------#
 
 # coco json文件
 json_file = "D:/ml/code/datasets/coco/annotations/instances_val2017.json"
 # 保存txt目录
-save_dir  = "D:/ml/code/datasets/coco/annotations/val_txt"
+save_dir = "D:/ml/code/datasets/coco/annotations/val_txt"
 # bbox or segmentation
-tp        = "bbox"
+tp = "bbox"
 
 
 def coco2yolo():
@@ -39,25 +39,25 @@ def coco2yolo():
     if tp == "bbox":
         bbox(js["annotations"], images)
     elif tp == "segmentation":
-        sementation(js["annotations"], images)
+        segmentation(js["annotations"], images)
 
 
 def bbox(annotations, images):
     for annotation in annotations:
-        image_id    = annotation["image_id"]
+        image_id = annotation["image_id"]
         category_id = annotation["category_id"]
-        height      = images[image_id]["height"]
-        width       = images[image_id]["width"]
+        height = images[image_id]["height"]
+        width = images[image_id]["width"]
 
         # bbox
-        bbox        = annotation["bbox"]            # coco: [x_min, ymin, width, height]
-        bbox[0]    += int(bbox[2] / 2)              # yolo: [x_center, y_center, width, height]
-        bbox[1]    += int(bbox[3] / 2)
-        x_center    = round(bbox[0] / width, 6)     # 归一化
-        y_center    = round(bbox[2] / width, 6)
-        width_      = round(bbox[1] / height, 6)
-        height_     = round(bbox[3] / height, 6)
-        bbox_line   = [str(category_id), str(x_center), str(y_center), str(width_), str(height_)]
+        bbox = annotation["bbox"]  # coco: [x_min, ymin, width, height]
+        bbox[0] += int(bbox[2] / 2)  # yolo: [x_center, y_center, width, height]
+        bbox[1] += int(bbox[3] / 2)
+        x_center = round(bbox[0] / width, 6)  # 归一化
+        y_center = round(bbox[2] / width, 6)
+        width_ = round(bbox[1] / height, 6)
+        height_ = round(bbox[3] / height, 6)
+        bbox_line = [str(category_id), str(x_center), str(y_center), str(width_), str(height_)]
         if "bbox_lines" not in images[image_id].keys():
             images[image_id]["bbox_lines"] = [bbox_line]
         else:
@@ -68,7 +68,7 @@ def bbox(annotations, images):
     # 将lines的标注批量保存进以图片名字命名的txt文件中
     for image in tqdm(images.values()):
         num_pic += 1
-        if "bbox_lines" in image.keys(): # 有的图片没有box,就忽略lines
+        if "bbox_lines" in image.keys():  # 有的图片没有box,就忽略lines
             num_pic_bbox += 1
             num_bbox += len(image["bbox_lines"])
             file_name = "".join(image["file_name"].split(".")[:-1]) + ".txt"
@@ -80,38 +80,40 @@ def bbox(annotations, images):
             empty_images.append(image["file_name"])
 
     print("=" * 100)
-    print(f"总图片数量为 {num_pic}, 有bbox的图片数量为 {num_pic_bbox}, 没有bbox的图片数量为 {num_pic - num_pic_bbox}, 总bbox数为 {num_bbox}")
+    print(
+        f"总图片数量为 {num_pic}, 有bbox的图片数量为 {num_pic_bbox}, 没有bbox的图片数量为 {num_pic - num_pic_bbox}, 总bbox数为 {num_bbox}"
+    )
     print("=" * 100)
     print("没有bbox的图片为:", empty_images)
     print("=" * 100)
 
 
-def sementation(annotations, images):
+def segmentation(annotations, images):
     for annotation in annotations:
-        image_id    = annotation["image_id"]
+        image_id = annotation["image_id"]
         category_id = annotation["category_id"]
-        height      = images[image_id]["height"]
-        width       = images[image_id]["width"]
+        height = images[image_id]["height"]
+        width = images[image_id]["width"]
 
         # bbox
-        bbox        = annotation["bbox"]            # coco: [x_min, ymin, width, height]
-        bbox[0]    += int(bbox[2] / 2)              # yolo: [x_center, y_center, width, height]
-        bbox[1]    += int(bbox[3] / 2)
-        x_center    = round(bbox[0] / width, 6)     # 归一化
-        y_center    = round(bbox[2] / width, 6)
-        width_      = round(bbox[1] / height, 6)
-        height_     = round(bbox[3] / height, 6)
-        bbox_line   = [str(category_id), str(x_center), str(y_center), str(width_), str(height_)]
+        bbox = annotation["bbox"]  # coco: [x_min, ymin, width, height]
+        bbox[0] += int(bbox[2] / 2)  # yolo: [x_center, y_center, width, height]
+        bbox[1] += int(bbox[3] / 2)
+        x_center = round(bbox[0] / width, 6)  # 归一化
+        y_center = round(bbox[2] / width, 6)
+        width_ = round(bbox[1] / height, 6)
+        height_ = round(bbox[3] / height, 6)
+        bbox_line = [str(category_id), str(x_center), str(y_center), str(width_), str(height_)]
 
         # 忽略 segmentation 为字典的数据,里面存储的为 "segmentation": {"counts": [], "size": []},含义不明
         if isinstance(annotation["segmentation"], dict):
             continue
         for segmentation in annotation["segmentation"]:
             segment = np.array(segmentation)
-            segment[0::2] /= width                                      # 归一化
+            segment[0::2] /= width  # 归一化
             segment[1::2] /= height
             segment = list(segment)
-            segment = bbox_line + [str(round(s, 6)) for s in segment]   # 添加bbox
+            segment = bbox_line + [str(round(s, 6)) for s in segment]  # 添加bbox
             if "segment_lines" not in images[image_id].keys():
                 images[image_id]["segment_lines"] = [segment]
             else:
@@ -122,7 +124,7 @@ def sementation(annotations, images):
     # 将lines的标注批量保存进以图片名字命名的txt文件中
     for image in tqdm(images.values()):
         num_pic += 1
-        if "segment_lines" in image.keys(): # 有的图片没有box,就忽略lines
+        if "segment_lines" in image.keys():  # 有的图片没有box,就忽略lines
             num_pic_segment += 1
             num_segment += len(image["segment_lines"])
             file_name = "".join(image["file_name"].split(".")[:-1]) + ".txt"
@@ -133,7 +135,9 @@ def sementation(annotations, images):
             empty_images.append(image["file_name"])
 
     print("=" * 100)
-    print(f"总图片数量为 {num_pic}, 有segmentation的图片数量为 {num_pic_segment}, 没有segmentation的图片数量为 {num_pic - num_pic_segment}, 总segmentation数为 {num_segment}")
+    print(
+        f"总图片数量为 {num_pic}, 有segmentation的图片数量为 {num_pic_segment}, 没有segmentation的图片数量为 {num_pic - num_pic_segment}, 总segmentation数为 {num_segment}"
+    )
     print("=" * 100)
     print("没有segmentation的图片为:", empty_images)
     print("=" * 100)
